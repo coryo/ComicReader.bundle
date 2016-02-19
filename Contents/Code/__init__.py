b698c95d4ea9fd74345a8934e4a15ab5d36a8f05
Cory Parsons <babylonstudio@gmail.com>
2016-02-19 00:47:25 -0500

    basic auto bookmarking

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,7 +3,8 @@
 # unrar binary loading based on https://github.com/sharkone/BitTorrent.bundle
 
 import os
-
+import json
+from __builtin__ import open
 
 NAME = 'ComicReader'
 PREFIX = '/photos/comicreader'
@@ -11,6 +12,7 @@ PREFIX = '/photos/comicreader'
 
 def Start():
     Route.Connect(PREFIX + '/getimage', SharedCodeService.formats.get_image)
+    Route.Connect(PREFIX + '/getthumb', SharedCodeService.formats.get_thumb)
     Route.Connect(PREFIX + '/getcover', SharedCodeService.formats.get_cover)
     SharedCodeService.formats.init_rar(Core.bundle_path)
     ObjectContainer.title1 = NAME
@@ -18,12 +20,21 @@ def Start():
 
 @handler(PREFIX, NAME)
 def MainMenu():
-    return BrowseDir(Prefs['cb_path'])
+    oc = ObjectContainer(no_cache=True)
+    with open(SharedCodeService.formats.DB_FILE, 'r') as f:
+        db = json.loads(f.read())
+        if Request.Headers['X-Plex-Token'] in db:
+            archive, filename, fmt = db[Request.Headers['X-Plex-Token']]
+            title = archive.split('/')[-1].split('\\')[-1]
+            oc.add(DirectoryObject(key=Callback(Comic, archive=archive, fmt=fmt, filename=filename), title='>> resume {}'.format(title)))
+    for x in BrowseDir(Prefs['cb_path']).objects:
+        oc.add(x)
+    return oc
 
 
 @route(PREFIX + '/browse')
 def BrowseDir(cur_dir):
-    oc = ObjectContainer()
+    oc = ObjectContainer(no_cache=True)
     try:
         dir_list = os.listdir(cur_dir)
     except Exception:
@@ -42,7 +53,7 @@ def BrowseDir(cur_dir):
                 continue
             oc.add(DirectoryObject(key=Callback(Comic, archive=full_path,
                                                 fmt=fmt),
-                                   title=os.path.splitext(item)[0],
+                                   title='[{}] {}'.format(fmt, os.path.splitext(item)[0]),
                                    thumb=Callback(SharedCodeService.formats.get_cover,
                                                   archive=full_path,
                                                   fmt=fmt)))
@@ -50,19 +61,22 @@ def BrowseDir(cur_dir):
 
 
 @route(PREFIX + '/comic')
-def Comic(archive, fmt):
-    oc = ObjectContainer(title2=unicode(archive))
+def Comic(archive, fmt, filename=None):
+    oc = ObjectContainer(title2=unicode(archive), no_cache=True)
     a = SharedCodeService.formats.get_archive(archive, fmt)
-    for f in a.namelist():
+    files = sorted(a.namelist())
+    if filename is not None:
+        pos = files.index(filename)
+        files = files[max(0, pos - 3):]
+    for f in files:
         if f.endswith('/'):
             continue
         Log.Info(f)
-        oc.add(PhotoObject(url='comicreader://{}|{}|{}'.format(archive, f, fmt),
-                           title=unicode(f),
-                           thumb=Callback(SharedCodeService.formats.get_image,
+        page = f.split('/')[-1] if '/' in f else f
+        oc.add(PhotoObject(url='comicreader://{}|{}|{}|{}'.format(archive, f, fmt, Request.Headers['X-Plex-Token']),
+                           title=unicode(page) if f != filename else '>> {}'.format(page),
+                           thumb=Callback(SharedCodeService.formats.get_thumb,
                                           archive=archive,
                                           filename=f,
-                                          fmt=fmt)
-                           ))
-    oc.objects.sort(key=lambda obj: obj.title)
+                                          fmt=fmt)))
     return oc
