0756e6386a9086e1a3a1ff14756d4131a5374069
Cory Parsons <babylonstudio@gmail.com>
2016-05-14 19:47:27 +0000

    cleanup

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,6 +6,7 @@ import os
 import hashlib
 import random
 import re
+import time
 from io import open
 
 from updater import Updater
@@ -20,75 +21,135 @@ def error_message(error, message):
     return MessageContainer(header=unicode(error), message=unicode(message))
 
 
-def get_session_identifier():
-    """Return a string that can consistently identify the current user of the channel"""
+def Start():
+    Route.Connect(PREFIX + '/getimage', formats.get_image)
+    Route.Connect(PREFIX + '/getthumb', formats.get_thumb)
+    Route.Connect(PREFIX + '/getcover', formats.get_cover)
+    ObjectContainer.title1 = NAME
+
+    if 'usernames' not in Dict:
+        Dict['usernames'] = {}
+
+    if 'read_states' not in Dict:
+        Dict['read_states'] = {}
+
+    if 'resume_states' not in Dict:
+        Dict['resume_states'] = {}
+
+
+@handler(PREFIX, NAME)
+def MainMenu():
+    formats.init_rar(Prefs['unrar'])
+    formats.init_sz(Prefs['seven_zip'])
+
+    user = formats.get_session_identifier()
+    Log.Info('USER: {}'.format(user))
+
+    if user not in Dict['read_states']:
+        Dict['read_states'][user] = {}
+
+    oc = ObjectContainer(no_cache=True)
+    if bool(Prefs['update']):
+        Updater(PREFIX + '/updater', oc)
+
+    for x in BrowseDir(Prefs['cb_path'], page_size=int(Prefs['page_size']), user=user).objects:
+        oc.add(x)
+    return oc
+
+
+@route(PREFIX + '/browse', page_size=int, offset=int)
+def BrowseDir(cur_dir, page_size=20, offset=0, user=None):
+    """Browse directories, with paging. Show directories and compatible archives."""
+    oc = ObjectContainer(no_cache=True)
     try:
-        h = hashlib.sha1(Request.Headers['X-Plex-Token']).hexdigest()
-        if h in Dict['usernames']:
-            return Dict['usernames'][h]
-        else:
-            Dict['usernames'][h] = get_username(Request.Headers['X-Plex-Token'])
-            Dict.Save()
-            return Dict['usernames'][h]
+        dir_list = formats.filtered_listdir(cur_dir)
+        page = dir_list[offset:offset + page_size]
     except Exception as e:
         Log.Error(e)
-        return Request.Headers.get('X-Plex-Token', 'none')
+        return error_message('bad path', 'bad path')
+    for item, is_dir in page:
+        full_path = os.path.join(cur_dir, item)
+        if is_dir:
+            oc.add(DirectoryObject(
+                key=Callback(BrowseDir, cur_dir=full_path, page_size=page_size, user=user),
+                title=unicode(item),
+                thumb=R('folder.png')))
+        else:
+            state = formats.read(user, full_path)
+            title = os.path.splitext(item)[0]
+            oc.add(DirectoryObject(
+                key=Callback(ComicMenu, archive=full_path, title=title, user=user),
+                title=unicode(formats.decorate_title(full_path, user, state, title)),
+                thumb=formats.thumb_transcode(Callback(formats.get_cover, archive=full_path))))
+
+    if offset + page_size < len(dir_list):
+        oc.add(NextPageObject(key=Callback(BrowseDir, cur_dir=cur_dir,
+                              page_size=page_size, offset=offset + page_size, user=user)))
+    return oc
 
 
-def thumb_transcode(url, w=150, h=150):
-    """ use the PMS photo transcoder for thumbnails """
-    return '/photo/:/transcode?url={}&height={}&width={}&maxSize=1'.format(String.Quote(url), w, h)
+@route(PREFIX + '/comic/menu')
+def ComicMenu(archive, title, user=None):
+    """The 'main menu' for a comic. this allows for different functions to be added."""
+    oc = ObjectContainer(title2=unicode(archive), no_cache=True)
+    state = formats.read(user, archive)
+    # Full comic
+    oc.add(PhotoAlbumObject(
+        key=Callback(Comic, archive=archive, user=user),
+        rating_key=hashlib.md5(archive).hexdigest(),
+        title=unicode(formats.decorate_title(archive, user, state, title)),
+        thumb=formats.thumb_transcode(Callback(formats.get_cover,
+                                               archive=archive))))
+    # Resume
+    if state == formats.State.IN_PROGRESS:
+        cur, total = formats.status(user, archive)
+        oc.add(DirectoryObject(title=unicode(L('resume')), thumb=R('resume.png'),
+                               key=Callback(Comic, archive=archive, user=user, page=cur)))
+    # Read/Unread toggle
+    if state == formats.State.UNREAD or state == formats.State.IN_PROGRESS:
+        oc.add(DirectoryObject(title=unicode(L('mark_read')), thumb=R('mark-read.png'),
+                               key=Callback(MarkRead, user=user, archive=archive)))
+    else:
+        oc.add(DirectoryObject(title=unicode(L('mark_unread')), thumb=R('mark-unread.png'),
+                               key=Callback(MarkUnread, user=user, archive=archive)))
+    return oc
 
 
-def get_username(token):
-    """retrieve the username for the given access token from plex.tv"""
-    access_tokens = XML.ElementFromURL(
-        'https://plex.tv/servers/{}/access_tokens.xml?auth_token={}'.format(
-            Core.get_server_attribute('machineIdentifier'), os.environ['PLEXTOKEN']),
-        cacheTime=CACHE_1HOUR)
-    for child in access_tokens.getchildren():
-        if child.get('token') == token:
-            username = child.get('username')
-            return username if username else child.get('title')
-    return token
-
-
-def filtered_listdir(directory):
-    """Return a list of only directories and compatible format files in `directory`"""
-    dirs, comics = [], []
-    for x in sorted_nicely(os.listdir(directory)):
-        if os.path.isdir(os.path.join(directory, x)):
-            if bool(Prefs['dirs_first']):
-                dirs.append((x, True))
-            else:
-                comics.append((x, True))
-        elif os.path.splitext(x)[-1] in formats.FORMATS:
-            comics.append((x, False))
-    return dirs + comics
-
-
-def sorted_nicely(l):
-    """sort file names as you would expect them to be sorted"""
-    def alphanum_key(key):
-        return [int(c) if c.isdigit() else c for c in re.split('([0-9]+)', key.lower())]
-    return sorted(l, key=alphanum_key)
-
-
-def read(user, archive):
-    """Return the read state of archive for the session user"""
+@route(PREFIX + '/comic', page=int)
+def Comic(archive, user=None, page=0):
+    """Return an oc with all pages in archive. if page > 0 return pages [page - Prefs['resume_length']:]"""
+    oc = ObjectContainer(title2=unicode(archive), no_cache=True)
     try:
-        cur, total = Dict['read_states'][user][unicode(archive)]
-        return formats.State.READ if abs(total - cur) < 5 else formats.State.IN_PROGRESS
-    except (KeyError, AttributeError):
-        return formats.State.UNREAD
+        a = formats.get_archive(archive)
+    except formats.ArchiveError as e:
+        Log.Error(e)
+        return error_message('bad archive', 'unable to open archive: {}'.format(archive))
+    for f in formats.sorted_nicely(a.namelist()):
+        if f.endswith('/'):  # we're flattening the archive structure, so don't list the dirs.
+            continue
+        decoration = None
+        if page > 0:
+            m = re.search(r'([0-9]+)([a-zA-Z])?\.', f)
+            if m:
+                page_num = int(m.group(1))
+                if page_num < page - int(Prefs['resume_length']):
+                    continue
+                if page_num <= page:
+                    decoration = '>'
+        page_title, ext = os.path.splitext(f)
+        page_title, ext = os.path.basename(page_title), ext[1:]
+        if decoration is not None:
+            page_title = '{} {}'.format(decoration, page_title)
 
-
-def status(user, archive):
-    try:
-        cur, total = Dict['read_states'][user][unicode(archive)]
-        return (int(cur), int(total))
-    except (KeyError, AttributeError):
-        return (0, 0)
+        oc.add(CreatePhotoObject(
+            media_key=Callback(GetImage, archive=String.Encode(archive),
+                               filename=String.Encode(f), user=user, extension=ext,
+                               time=int(time.time()) if bool(Prefs['prevent_caching']) else 0),
+            rating_key=hashlib.sha1('{}{}{}'.format(archive, f, user)).hexdigest(),
+            title=unicode(page_title),
+            thumb=formats.thumb_transcode(Callback(formats.get_thumb, archive=archive,
+                                                   filename=f))))
+    return oc
 
 
 @route(PREFIX + '/markread')
@@ -113,155 +174,19 @@ def MarkUnread(user, archive):
     return error_message('marked', 'marked')
 
 
-###############################################################################
+@route(PREFIX + '/createphotoobject')
+def CreatePhotoObject(rating_key, title, thumb, media_key=None):
+    """simulate a url service"""
+    po = PhotoObject(
+        key=Callback(CreatePhotoObject, rating_key=rating_key, title=title, thumb=thumb),
+        rating_key=rating_key, title=title, thumb=thumb)
+    if media_key:
+        po.add(MediaObject(parts=[PartObject(key=media_key)]))
+    return po
 
 
-def Start():
-    Route.Connect(PREFIX + '/getimage', formats.get_image)
-    Route.Connect(PREFIX + '/getthumb', formats.get_thumb)
-    Route.Connect(PREFIX + '/getcover', formats.get_cover)
-    ObjectContainer.title1 = NAME
-
-    if 'usernames' not in Dict:
-        Dict['usernames'] = {}
-
-    if 'read_states' not in Dict:
-        Dict['read_states'] = {}
-
-    if 'resume_states' not in Dict:
-        Dict['resume_states'] = {}
-
-
-@handler(PREFIX, NAME)
-def MainMenu():
-    formats.init_rar(Prefs['unrar'])
-    formats.init_sz(Prefs['seven_zip'])
-
-    user = get_session_identifier()
-    if user not in Dict['read_states']:
-        Dict['read_states'][user] = {}
-
-    oc = ObjectContainer(no_cache=True)
-    if bool(Prefs['update']):
-        Updater(PREFIX + '/updater', oc)
-
-    for x in BrowseDir(Prefs['cb_path'], page_size=int(Prefs['page_size']), user=user).objects:
-        oc.add(x)
-    return oc
-
-
-def decorate_title(archive, user, state, title):
-    if state == formats.State.UNREAD:
-        indicator = Prefs['unread_symbol']
-    elif state == formats.State.IN_PROGRESS:
-        try:
-            indicator = '{} [{}/{}]'.format(Prefs['in_progress_symbol'], *Dict['read_states'][user][archive])
-        except Exception:
-            indicator = Prefs['in_progress_symbol']
-    elif state == formats.State.READ:
-        indicator = Prefs['read_symbol']
-    return '{} {}'.format('' if indicator is None else indicator.strip(), title)
-
-
-@route(PREFIX + '/browse', page_size=int, offset=int)
-def BrowseDir(cur_dir, page_size=20, offset=0, user=None):
-    oc = ObjectContainer(no_cache=True)
-    try:
-        dir_list = filtered_listdir(cur_dir)
-        page = dir_list[offset:offset + page_size]
-    except Exception as e:
-        Log.Error(e)
-        return error_message('bad path', 'bad path')
-    for item, is_dir in page:
-        full_path = os.path.join(cur_dir, item)
-        if is_dir:
-            oc.add(DirectoryObject(
-                key=Callback(BrowseDir, cur_dir=full_path, page_size=page_size, user=user),
-                title=unicode(item),
-                thumb=R('folder.png')))
-        else:
-            state = read(user, full_path)
-            title = unicode(os.path.splitext(item)[0])
-
-            oc.add(DirectoryObject(
-                key=Callback(ComicMenu, archive=full_path, title=title, user=user),
-                title=unicode(decorate_title(full_path, user, state, title)),
-                thumb=thumb_transcode(Callback(formats.get_cover, archive=full_path))))
-
-    if offset + page_size < len(dir_list):
-        oc.add(NextPageObject(key=Callback(BrowseDir, cur_dir=cur_dir,
-                              page_size=page_size, offset=offset + page_size, user=user)))
-    return oc
-
-
-@route(PREFIX + '/comic/menu')
-def ComicMenu(archive, title, user=None):
-    oc = ObjectContainer(title2=unicode(archive), no_cache=True)
-    state = read(user, archive)
-
-    oc.add(PhotoAlbumObject(
-        key=Callback(Comic, archive=archive, user=user),
-        rating_key=hashlib.md5(archive).hexdigest(),
-        title=unicode(decorate_title(archive, user, state, title)),
-        thumb=thumb_transcode(Callback(formats.get_cover,
-                                       archive=archive))))
-    if state == formats.State.IN_PROGRESS:
-        cur, total = status(user, archive)
-        oc.add(DirectoryObject(title=unicode(L('resume')), thumb=R('resume.png'),
-                               key=Callback(Comic, archive=archive, user=user, page=cur)))
-    if state == formats.State.UNREAD or state == formats.State.IN_PROGRESS:
-        oc.add(DirectoryObject(title=unicode(L('mark_read')), thumb=R('mark-read.png'),
-                               key=Callback(MarkRead, user=user, archive=archive)))
-    else:
-        oc.add(DirectoryObject(title=unicode(L('mark_unread')), thumb=R('mark-unread.png'),
-                               key=Callback(MarkUnread, user=user, archive=archive)))
-    return oc
-
-
-@route(PREFIX + '/comic', page=int)
-def Comic(archive, user=None, page=0):
-    oc = ObjectContainer(title2=unicode(archive), no_cache=True)
-    try:
-        a = formats.get_archive(archive)
-    except formats.ArchiveError as e:
-        Log.Error(e)
-        return error_message('bad archive', 'unable to open archive: {}'.format(archive))
-    files = a.namelist()
-
-    for f in sorted_nicely(files):
-        if f.endswith('/'):
-            continue
-        decoration = None
-        if page > 0:
-            m = re.search(r'([0-9]+)([a-zA-Z])?\.', f)
-            if m:
-                page_num = int(m.group(1))
-                if page_num < page - int(Prefs['resume_length']):
-                    continue
-                if page_num == page:
-                    decoration = '>'
-        page_title = f.split('/')[-1] if '/' in f else f
-        if decoration is not None:
-            page_title = '{} {}'.format(decoration, page_title)
-        ext = f.split('.')[-1]
-        rating_key = hashlib.sha1('{}{}{}'.format(archive, f, user)).hexdigest()
-        cb = Callback(GetImage, archive=String.Encode(archive), filename=String.Encode(f), user=user, extension=ext)
-        po = PhotoObject(key=Callback(MetadataObject, key=cb, rating_key=rating_key),
-                         rating_key=rating_key,
-                         title=unicode(page_title),
-                         thumb=thumb_transcode(Callback(formats.get_thumb,
-                                                        archive=archive,
-                                                        filename=f)))
-        po.add(MediaObject(parts=[PartObject(key=cb)]))
-        oc.add(po)
-    return oc
-
-
-@route(PREFIX + '/metadata')
-def MetadataObject(key, rating_key):
-    return PhotoObject(key=key, rating_key=rating_key, title='x', summary='x')
-
-
-@route(PREFIX + '/image/{user}/{archive}/{filename}.{extension}')
-def GetImage(archive, filename, user, extension):
+@route(PREFIX + '/image/{user}/{archive}/{filename}.{extension}', time=int)
+def GetImage(archive, filename, user, extension, time=0):
+    """direct response with image data. setting a new time value should prevent
+    clients from loading their cached copy so our page tracking code can run."""
     return formats.get_image(String.Decode(archive), String.Decode(filename), user)
--- Contents/Code/formats.py
+++ Contents/Code/formats.py
@@ -105,3 +105,85 @@ def get_cover(archive):
         x = sorted([x for x in a.namelist() if not x.endswith('/')])
         if x:
             return DataObject(a.read(x[0]), mime_type(x[0]))
+
+
+def get_session_identifier():
+    """Return a string that can consistently identify the current user of the channel"""
+    h = hashlib.sha1(Request.Headers.get('X-Plex-Token', 'none')).hexdigest()
+    try:
+        if h in Dict['usernames']:
+            return Dict['usernames'][h]
+        else:
+            Dict['usernames'][h] = get_username(Request.Headers['X-Plex-Token'])
+            Dict.Save()
+            return Dict['usernames'][h]
+    except Exception as e:
+        Log.Error('get_session_identifier: {}'.format(e))
+        return h
+
+
+def thumb_transcode(url, w=150, h=150):
+    """use the PMS photo transcoder for thumbnails"""
+    return '/photo/:/transcode?url={}&height={}&width={}&maxSize=1'.format(String.Quote(url), w, h)
+
+
+def get_username(token):
+    """retrieve the username for the given access token from plex.tv"""
+    access_tokens = XML.ElementFromURL(
+        'https://plex.tv/servers/{}/access_tokens.xml?auth_token={}'.format(
+            Core.get_server_attribute('machineIdentifier'), os.environ['PLEXTOKEN']),
+        cacheTime=CACHE_1HOUR)
+    for child in access_tokens.getchildren():
+        if child.get('token') == token:
+            username = child.get('username')
+            return username if username else child.get('title')
+    return token
+
+
+def decorate_title(archive, user, state, title):
+    if state == State.UNREAD:
+        indicator = Prefs['unread_symbol']
+    elif state == State.IN_PROGRESS:
+        try:
+            indicator = '{} [{}/{}]'.format(Prefs['in_progress_symbol'], *status(user, archive))
+        except Exception:
+            indicator = Prefs['in_progress_symbol']
+    elif state == State.READ:
+        indicator = Prefs['read_symbol']
+    return '{} {}'.format('' if indicator is None else indicator.strip(), title)
+
+
+def filtered_listdir(directory):
+    """Return a list of only directories and compatible format files in `directory`"""
+    dirs, comics = [], []
+    for x in sorted_nicely(os.listdir(directory)):
+        if os.path.isdir(os.path.join(directory, x)):
+            l = dirs if bool(Prefs['dirs_first']) else comics
+            l.append((x, True))
+        elif os.path.splitext(x)[-1] in FORMATS:
+            comics.append((x, False))
+    return dirs + comics
+
+
+def sorted_nicely(l):
+    """sort file names as you would expect them to be sorted"""
+    def alphanum_key(key):
+        return [int(c) if c.isdigit() else c for c in re.split('([0-9]+)', key.lower())]
+    return sorted(l, key=alphanum_key)
+
+
+def read(user, archive, fuzz=5):
+    """Return the read state of archive for the session user"""
+    try:
+        cur, total = Dict['read_states'][user][unicode(archive)]
+        return State.READ if abs(total - cur) < fuzz else State.IN_PROGRESS
+    except (KeyError, AttributeError):
+        return State.UNREAD
+
+
+def status(user, archive):
+    try:
+        cur, total = Dict['read_states'][user][unicode(archive)]
+        return (int(cur), int(total))
+    except (KeyError, AttributeError):
+        return (0, 0)
